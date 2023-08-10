536f08363bc29256636616d7f5c7686e94280640
sofia trindade <sofiaatrindadee02@gmail.com>
2023-08-10 16:34:24 -0300

    adiciona Head nas pg. /profile e /posts/first-post

--- pages/profile.js
+++ pages/profile.js
@@ -1,10 +1,19 @@
 import Image from 'next/image';
+import Head from 'next/head';
 
 export default function Profile () {
-    return <Image
+    return (
+        <>
+        <Head>
+            <title>First Post</title>
+        </Head>
+
+        <Image
                 src="/images/profile.jpg" 
                 height={144}
                 width={144}
                 alt="Your Profile" 
             />
+            </>
+    );
 }
